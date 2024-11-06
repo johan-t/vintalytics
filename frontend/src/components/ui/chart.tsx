ee8208c79b76a59328102749173b6b669bc092c8
David Brandes <brandes.1@campus.tu-berlin.de>
2024-11-06 22:07:17 +0100

    make chart tooltip text black

--- frontend/src/components/ui/chart.tsx
+++ frontend/src/components/ui/chart.tsx
@@ -179,7 +179,7 @@ const ChartTooltipContent = React.forwardRef<
       <div
         ref={ref}
         className={cn(
-          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl text-black",
           className
         )}
       >
@@ -234,12 +234,12 @@ const ChartTooltipContent = React.forwardRef<
                     >
                       <div className="grid gap-1.5">
                         {nestLabel ? tooltipLabel : null}
-                        <span style={{ color: "rgb(244 244 245)" }}>
+                        <span className="text-black">
                           {itemConfig?.label || item.name}
                         </span>
                       </div>
                       {item.value && (
-                        <span className="font-mono font-medium tabular-nums text-foreground">
+                        <span className="font-mono font-medium tabular-nums text-black">
                           {item.value.toLocaleString()}
                         </span>
                       )}
